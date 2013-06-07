f045c7561a2cd35b60c37e3f0fa6bf97f3edd193
Minkoo Seo <minkoo.seo@gmail.com>
2013-06-08 01:14:17 +0900

    Fix indentation.

--- slopeone.R
+++ slopeone.R
@@ -13,7 +13,7 @@ require(reshape2)
 #   ...: Extra parameters for ddply.
 #
 # Returns:
-#   A data table of (item_id1, item_id2, b, support) where b represents the 
+#   A data table of (item_id1, item_id2, b, support) where b represents the
 #   average rating difference of 'item 2 rating' - 'item 1 rating'. support
 #   represents number of ratings used to compute b.
 build_slopeone <- function(ratings, ...) {
@@ -24,9 +24,9 @@ build_slopeone <- function(ratings, ...) {
   # 2 is rated by the same user.
   score_diff_per_user <- dlply(ratings, .(user_id), function(rows) {
     if (NROW(rows) > 1) {
-      # A user may have rated an item multiple times. In such case, get average ratings 
+      # A user may have rated an item multiple times. In such case, get average ratings
       # for such items.
-      rows <- unique(ddply(rows, .(item_id), transform, rating=mean(rating)))  
+      rows <- unique(ddply(rows, .(item_id), transform, rating=mean(rating)))
       # Compute diff of every pair of items.
       pair_rows_nums <- subset(
           expand.grid(rows_num1=1:NROW(rows), rows_num2=1:NROW(rows)),
@@ -34,17 +34,17 @@ build_slopeone <- function(ratings, ...) {
       data.table(
           item_id1=rows[pair_rows_nums$rows_num1, 'item_id'],
           item_id2=rows[pair_rows_nums$rows_num2, 'item_id'],
-          diff=rows[pair_rows_nums$rows_num2, 'rating'] 
+          diff=rows[pair_rows_nums$rows_num2, 'rating']
               - rows[pair_rows_nums$rows_num1, 'rating'])
     }
   }, ...)
-  # ddply is slow when merging data frames within list while rbindlist is 
+  # ddply is slow when merging data frames within list while rbindlist is
   # much faster.
   score_diff_per_user <- rbindlist(score_diff_per_user)
   score_diff_per_user$item_id1 <- as.character(score_diff_per_user$item_id1)
   score_diff_per_user$item_id2 <- as.character(score_diff_per_user$item_id2)
   # Compute average score diff between item 1 and item 2.
-  model <- score_diff_per_user[, 
+  model <- score_diff_per_user[,
                                list(b=mean(diff), support=NROW(diff)),
                                by='item_id1,item_id2']
   setkey(model, item_id1, item_id2)
@@ -55,13 +55,13 @@ build_slopeone <- function(ratings, ...) {
 # Predict ratings for multiple users and items given known ratings.
 #
 # Param:
-#  model: A data table produced by build_slopeone.
-#  targets: A data table of (user_id, item_id) to predict ratings.
-#  ratings: A data table containig (user_id, item_id, rating) of known
-#    ratings.
-#  ...: Extra parameters for adply.
+#   model: A data table produced by build_slopeone.
+#   targets: A data table of (user_id, item_id) to predict ratings.
+#   ratings: A data table containig (user_id, item_id, rating) of known
+#     ratings.
+#   ...: Extra parameters for adply.
 # Returns:
-#  A data table containig (user_id, item_id, predicted_rating).
+#   A data table containig (user_id, item_id, predicted_rating).
 predict_slopeone <-function(model, targets, ratings, ...) {
   setkey(ratings, user_id)
   adply(targets,
@@ -71,7 +71,7 @@ predict_slopeone <-function(model, targets, ratings, ...) {
               predicted_rating=predict_slopeone_for_user(
                   model, row$item_id, ratings[J(row$user_id), ]))
         }, ...)
-} 
+}
 
 # Predict score for target_item_id given the known ratings of a single user.
 #
@@ -89,7 +89,7 @@ predict_slopeone_for_user <- function(model, target_item_id, ratings) {
   if (NROW(already_rated) == 1) {
     return(already_rated$rating)
   } else if (NROW(already_rated) > 1) {
-    warning(paste(target_item_id, 
+    warning(paste(target_item_id,
                   ' is already rated by user, but there are multiple ratings.'))
     return(already_rated[1, ]$rating)
   }
